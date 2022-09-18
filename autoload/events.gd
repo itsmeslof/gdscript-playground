cf187c4e5248235079c7e77c4e47c81535e11afc
Slof <slof@hey.com>
2022-09-18 16:56:10 -0500

    Create events.gd

---
+++ autoload/events.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+signal run_script_requested(script)
+signal clear_output_requested()
+
+signal user_script_compiled(script)
+
+signal custom_print_func_called(text)
+signal compile_failed()
